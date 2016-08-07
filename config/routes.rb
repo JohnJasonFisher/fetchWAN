1584ef4600f65492c3a9bd9a70ba077b5e8f7d58
Profession-Programing <jjfisher91@gmail.com>
2016-08-07 15:09:05 -0500

    Users able to log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get '/cards' => 'cards#index'
   get '/cards/:id' => 'cards#show'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login/' => 'sessions#destroy'
 end
