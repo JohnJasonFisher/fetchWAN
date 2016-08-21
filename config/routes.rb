5d7e919df52281cbb094597ee912924d690d80d4
Profession-Programing <jjfisher91@gmail.com>
2016-08-21 14:08:10 -0500

    formatted logout header

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  delete '/logout/' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
 
   get '/cardusers/new' => 'card_users#new'
   post '/cardusers' => 'card_users#create'
