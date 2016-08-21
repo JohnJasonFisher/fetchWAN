bd53a785fa7fef1fe79a6cc6561079fdb0df9107
Profession-Programing <jjfisher91@gmail.com>
2016-08-21 15:18:46 -0500

    price from api available

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,8 @@
 class Card < ActiveRecord::Base
   has_many :card_users
   has_many :users, through: :card_users
+
+  def price
+    Unirest.get("http://magictcgprices.appspot.com/api/cfb/price.json?cardname=#{name}").body[0]
+  end
 end
