c77b45ded8d788f726dfce5786c555dd724c0586
Profession-Programing <jjfisher91@gmail.com>
2016-08-21 15:22:37 -0500

    Bold Price Shows

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,6 +3,11 @@ class Card < ActiveRecord::Base
   has_many :users, through: :card_users
 
   def price
-    Unirest.get("http://magictcgprices.appspot.com/api/cfb/price.json?cardname=#{name}").body[0]
+    price = Unirest.get("http://magictcgprices.appspot.com/api/cfb/price.json?cardname=#{name}").body[0]
+    if price == ""
+      return "Price not available"
+    else
+      return price
+    end
   end
 end
