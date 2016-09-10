a6c3c1ea9988de7c388656299e3d46e77c45aad4
Profession-Programing <jjfisher91@gmail.com>
2016-09-10 06:55:15 -0500

    job setup, needs testing

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,10 @@
 #   runner "Card.record_retail_price", environment: "development"
 # end
 
-# every 1.minute do
-#   runner "Card.recond_market_price", environment: "development"
-# end
+every 1.day, :at => '7:00 pm' do
+  runner "Card.record_market_price", environment: "development"
+end
+
+every 1.day, :at => '7:00 am' do
+  runner "Card.record_market_price", environment: "development"
+end
