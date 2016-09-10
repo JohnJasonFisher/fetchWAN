46c2c9fc62f4eee23f86811adefd8c87f3573af1
Profession-Programing <jjfisher91@gmail.com>
2016-09-10 09:11:47 -0500

    self.send_message

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,28 +1,14 @@
 class Alert < ActiveRecord::Base
   belongs_to :user
 
-  def alert_seller
-    @client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
+  def self.send_message
+    @twilio_number = ENV['TWILIO_NUMBER_PHONE_NUMBER']
+    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 
-    @message = @client.messages.create(
-      to: user.phone_number,
-      # to: "+18152050931",
-      from: "+18152050931",
-      body: "Hello, you are recieving this text because you have a card that is worth your sell price."
+    message = @client.account.messages.create(
+      :from => @twilio_number,
+      :to => ENV['MY_PHONE_NUMBER'],
+      :body => 'TESTING 1,2,3'
     )
-  end
-
-  def alert_admin
-    @client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
-
-    @message = @client.messages.create(
-      to: "+18152050931",
-      from: "+18152050931",
-      body: "Price change yo."
-    ) 
-  end
-
-  def user
-    User.find_by(id: user_id)
   end
 end
