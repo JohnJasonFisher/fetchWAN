84cb6ee2808e2a550ef2c09f292c66dbd3f3d563
Profession-Programing <jjfisher91@gmail.com>
2016-09-10 00:03:01 -0500

    working market api method

--- app/models/card.rb
+++ app/models/card.rb
@@ -24,18 +24,22 @@ class Card < ActiveRecord::Base
     name = data["card"]["set"]
   end
 
+  def show_image
+    image = data["card"]["imageUrl"]
+  end
+
   def pull_retail_price
     price = Unirest.get("http://magictcgprices.appspot.com/api/cfb/price.json?cardname=#{name}&setname=#{set_name}").body[0]
-    if price == ""
-      return "Price not available"
+    if price == "" || '0.00'
+      return 'price not available'
     else
-      return price[1..-1].to_f
+      return price[0..-1].to_f
     end
   end
 
   def pull_market_price
-    price = Unirest.get("http://api.mtgowikiprice.com/api/card/price?sets=#{set_name}&cardNames=#{name}&api_key=API_KEY").body
-    price = price.split(';')[2][1..-1].to_f
+    price = Unirest.get("http://api.mtgowikiprice.com/api/card/price?sets=#{set}&cardNames=#{name}&api_key=#{ENV['API_KEY']}").body
+    price = price.split(';')[2][0..-1].to_f
   end
 
   def self.record_retail_price
@@ -48,17 +52,15 @@ class Card < ActiveRecord::Base
     end
   end
 
-  def self.recond_market_price
+  def self.record_market_price
     cards = self.all
     cards.each do |card|
       if card.pull_market_price != card.current_price
+        puts card.pull_market_price
         card.current_price = card.pull_market_price
         card.save
+        puts card.pull_market_price
       end
     end
-  end
-
-  def show_image
-    image = data["card"]["imageUrl"]
   end
 end
