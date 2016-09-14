43cd8f0c27b973dd41e4406f4bdd6a0f6a218f56
Profession-Programing <jjfisher91@gmail.com>
2016-09-13 20:17:57 -0500

    cleaned stats page

--- app/assets/javascripts/Usercard_ctrl.js
+++ app/assets/javascripts/Usercard_ctrl.js
@@ -1,13 +1,11 @@
+/* global angular */
+
 (function() {
-  angular.module('app').controller('usercardCtrl', function($scope) {
-    $scope.message = "hello";
+  angular.module('app').controller('peopleCtrl', function($scope, $http) {
+    $scope.setup = function() {
+      $http.get('/api/v1/people.json').then(function(response) {
+        $scope.people = response.data;
+      });
+    };
   });
 })();
-
-angular.module('app').controller('peopleCtrl', function($scope, $http) {
-  $scope.setup = function() {
-    $http.get('/api/v1/people.json').then(function(response) {
-      $scope.people = response.data;
-    });
-  };
-}
