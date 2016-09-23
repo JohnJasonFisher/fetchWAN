c0a596788d158e65b28505f432efa757344e4f49
Profession-Programing <jjfisher91@gmail.com>
2016-09-23 16:57:25 -0500

    refactored record_market_prices

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,10 +3,6 @@ class Card < ActiveRecord::Base
   has_many :users, through: :card_users
   has_many :prices
 
-  # def api_hash
-  #   @api_hash ||
-  # end
-
   def price
     current_price
   end
@@ -32,15 +28,6 @@ class Card < ActiveRecord::Base
     image = data["card"]["imageUrl"]
   end
 
-  # def pull_retail_price
-  #   price = Unirest.get("http://magictcgprices.appspot.com/api/cfb/price.json?cardname=#{name}&setname=#{set_name}").body[0]
-  #   if price == "" || '0.00'
-  #     return 'price not available'
-  #   else
-  #     return price[0..-1].to_f
-  #   end
-  # end
-
   def pull_market_price
     price = Unirest.get("http://api.mtgowikiprice.com/api/card/price?sets=#{set}&cardNames=#{name}&api_key=#{ENV['API_KEY']}").body
     price = price.split(';')[2][0..-1].to_f
@@ -51,61 +38,27 @@ class Card < ActiveRecord::Base
     price
   end
 
-  # def self.record_retail_price
-  #   cards = self.all
-  #   cards.each do |card|
-  #     if card_price != card.current_price
-  #       card.current_price = card_price
-  #       card.save
-  #     end
-  #   end
-  # end
-
   def self.record_market_prices
     cards = self.all
     usercards = CardUser.all
     cards.each do |card|
       card_price = card.pull_market_price
-      usercards.each do |usercard|
-        card_price = card.pull_market_price
-        if card_price > usercard.desired_sell_price
-          Alert.alert_seller(usercard.user_id, usercard.user.phone_number, card.name)
-          puts 'MESSAGE SENT'
+      if card_price != card.current_price
+        puts "PRICE CHANGE !!!!! #{card_price}"
+        card.current_price = card_price
+        card.save
+        usercards.each do |usercard|
+          if card_price > usercard.desired_sell_price
+            Alert.alert_seller(usercard.user_id, usercard.user.phone_number, card.name)
+            puts 'MESSAGE SENT'
+          end
         end
-        if card_price != card.current_price
-          puts "PRICE CHANGE !!!!! #{card_price}"
-          card.current_price = card_price
-          card.save
-          puts card_price
-        end 
       end
     end
     puts 'finished'
   end
 
-  # def self.record_market_price
-  #   card = self.first
-  #   usercard = CardUser.first
-  #   card_price = card.pull_market_price
-  #   if usercard.desired_sell_price < card_price
-  #     Alert.alert_seller(usercard.user.name, usercard.user.phone_number)
-  #   end
-  #   if card_price != card.current_price
-  #     puts "PRICE CHANGE !!!!! #{card_price}"
-  #     card.current_price = card_price
-  #     card.save
-  #     puts card_price
-  #   end 
-  # end
-
   def growth_rate
-    # This returns all prices of one card in an array
-    # all_price = Price.where(card_id: id)
-    # all_market_prices = []
-    # all_price.each do |price|
-    #   all_market_prices << price.price
-    # end
-    # all_market_prices
     original_card_price = Price.where(card_id: id).order(created_at: :asc).first.price
     n = current_price - original_card_price
     d = original_card_price
@@ -116,7 +69,4 @@ class Card < ActiveRecord::Base
     growth_rate = n / d
   end
 
-  def self.growth_rate
-    
-  end
 end
