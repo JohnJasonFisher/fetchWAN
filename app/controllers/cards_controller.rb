bd49ed95d42f34973323e75e83b461fd32f71f4e
Profession-Programing <jjfisher91@gmail.com>
2017-01-15 03:32:13 -0600

    created 1 month graph

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,7 +2,8 @@ class CardsController < ApplicationController
   def show
     @usercard = CardUser.find_by(user_id: current_user.id, card_id: params[:id].to_i)
     @card = Card.find_by(id: params[:id])
-    # @price = Price.find_by(id:)
+    @all_price = Price.where(card_id: @card.id)
+    @month_ago_price = Price.where(card_id: @card.id, created_at: (Time.now - 1.month)..(Time.now))
     render "show.html.erb"
   end
 end
